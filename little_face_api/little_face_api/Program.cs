cf4b62e6447dc2224a701909a684410b8de2663b
Sammy Alexander Ayra <jayi.darksam@gmail.com>
2023-07-22 11:15:52 -0500

    cambios

--- little_face_api/little_face_api/Program.cs
+++ little_face_api/little_face_api/Program.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using little_face_api.Data;
+using Microsoft.OpenApi.Writers;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<little_face_DBContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("little_face_DBContext") ?? throw new InvalidOperationException("Connection string 'little_face_DBContext' not found.")));
@@ -11,6 +13,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<SeedDb>();
 
 var app = builder.Build();
 
@@ -20,6 +23,14 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var seedDb = services.GetRequiredService<SeedDb>();
+    await seedDb.SeedAsync();
+}
+
 
 app.UseHttpsRedirection();
 
