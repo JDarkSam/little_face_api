2b72dc5344cd41f9acf1a42f8fd488f5420a38a1
Sammy Alexander Ayra <jayi.darksam@gmail.com>
2023-08-10 23:10:15 -0500

    se crea modelo de infante

---
+++ little_face_api/little_face_api/Data/Models/Child.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace little_face_api.Data.Models
+{
+    public class Child
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
+
+        [Required]
+        public string Names { get; set; }
+
+        [Required]
+        public string Surnames { get; set; }
+
+        [Required]
+        public int Age { get; set; }
+
+        [Required]
+        public string Alias { get; set; }
+    }
+}
